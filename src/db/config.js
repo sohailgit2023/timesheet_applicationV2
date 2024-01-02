2314289e70c347d50ff8e8942aac96868ab6fd57
sohail <sohail_k@pursuitsoftware.biz>
2024-01-02 13:02:26 +0530

    xyz

--- src/db/config.js
+++ src/db/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://sohailkhan:sohailkhan@cluster0.flsvmxv.mongodb.net/timesheet_application?retryWrites=true&w=majority").then(result=>{
+mongoose.connect(
+    // "mongodb+srv://sohailkhan:sohailkhan@cluster0.flsvmxv.mongodb.net/timesheet_application?retryWrites=true&w=majority"
+    process.env.mongodbURL).then(result=>{
    console.log(process.env.mongodbURL);
 });
