a5bca689df5ada93ab30846cccbd61cabe1242ff
sohail <sohail_k@pursuitsoftware.biz>
2024-01-02 13:06:46 +0530

    xyz

--- src/db/config.js
+++ src/db/config.js
@@ -3,5 +3,4 @@ const mongoose=require('mongoose');
 mongoose.connect(
     // "mongodb+srv://sohailkhan:sohailkhan@cluster0.flsvmxv.mongodb.net/timesheet_application?retryWrites=true&w=majority"
     process.env.mongodbURL).then(result=>{
-   console.log(process.env.mongodbURL);
 });
