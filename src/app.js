abf0b74222b662c9ff8157a3d253045efb18c7b0
sohail <sohail_k@pursuitsoftware.biz>
2024-03-05 16:11:25 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -462,7 +462,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
+  if(req.method==='OPTIONS'){
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.status(200).end();
+  }
+  else{
+    next();
+  }
 });
   app.use(express.json());
     app.use(passport.initialize());
